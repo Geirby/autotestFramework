5ccc193504a0ecf141bfeeb35dc6a49007e2ae56
Geirby <m2.suboch@gmail.com>
2021-12-17 18:38:04 +0300

    Initial commit

---
+++ src/test/java/com/generaltest/utils/ConfigProperties.java
@@ -0,0 +1,32 @@
+package com.generaltest.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigProperties {
+    protected static FileInputStream fileInputStream;
+    protected static Properties PROPERTIES;
+    static String path = "src/test/resources/conf.properties";
+
+    static {
+        try {
+            fileInputStream = new FileInputStream(path);
+            PROPERTIES = new Properties();
+            PROPERTIES.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null)
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    public static String getProperty(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+}
